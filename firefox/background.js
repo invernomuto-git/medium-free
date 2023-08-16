fe6b93a6018f7c664a0fe56f036a3f42f94378ab
invernomuto-git <attesor@gmail.com>
2023-08-16 11:46:14 +0200

    UP

--- firefox/background.js
+++ firefox/background.js
@@ -3,13 +3,16 @@ browser.browserAction.onClicked.addListener(function( tab ) {
 	// Get the current tab
 	let currentTabUrl = tab.url;  
 
+	// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/create
 	browser.tabs.create({
 		//* COPIA CACHE NORMALE
 		url: "http://webcache.googleusercontent.com/search?q=cache:"+currentTabUrl+"",		
 		//* COPIA CACHE SOLO TESTO
 	  	// url: "http://webcache.googleusercontent.com/search?q=cache:"+currentTabUrl+"&strip=1",
-		//* COPIA SOLO CODICE
+		//* COPIA CACHE DEL SOLO CODICE
 		// url: "http://webcache.googleusercontent.com/search?q=cache:"+currentTabUrl+"&vwsrc=1",
+
+		//* APRE IN MODALITÀ READER
 		// openInReaderMode: true,
 	}); 
 });
