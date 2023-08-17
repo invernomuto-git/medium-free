587e44f475bf5aa2fee9dcd963e897f5c20e2886
invernomuto-git <attesor@gmail.com>
2023-08-17 17:37:31 +0200

    UP disable extension click on chrome://

--- chrome/background.js
+++ chrome/background.js
@@ -1,15 +1,16 @@
 chrome.action.onClicked.addListener(async function (sourceTab) {
 	// Get the current tab	
 	let currentTabUrl = sourceTab.url;
-	
-	chrome.tabs.create({
-		//* COPIA CACHE NORMALE
-		// url: "https://webcache.googleusercontent.com/search?q=cache:"+currentTabUrl+"",		
-		//* COPIA CACHE SOLO TESTO
-		// url: "http://webcache.googleusercontent.com/search?q=cache:"+currentTabUrl+"&strip=1",
-		//* COPIA CACHE DEL SOLO CODICE
-		url: "http://webcache.googleusercontent.com/search?q=cache:"+currentTabUrl+"&vwsrc=1",
-	});
+	if (!currentTabUrl.includes('chrome://')) {	
+		chrome.tabs.create({
+			//* COPIA CACHE NORMALE
+			// url: "https://webcache.googleusercontent.com/search?q=cache:"+currentTabUrl+"",		
+			//* COPIA CACHE SOLO TESTO
+			// url: "http://webcache.googleusercontent.com/search?q=cache:"+currentTabUrl+"&strip=1",
+			//* COPIA CACHE DEL SOLO CODICE
+			url: "http://webcache.googleusercontent.com/search?q=cache:"+currentTabUrl+"&vwsrc=1",
+		});
+	}
 });
 
 chrome.tabs.onCreated.addListener(function (tab) {
